7cdd8d25cd4d8664990fdeb56211cc7bdffa004e
Terho Uotila <terho.uotila@helsinki.fi>
2025-01-29 16:25:33 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_29_134630) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_29_142422) do
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "style"
+    t.integer "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
