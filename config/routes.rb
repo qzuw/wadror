3a423e8902887db4bdc3e1c95822789b868f51b2
Terho Uotila <terho.uotila@helsinki.fi>
2025-02-01 19:18:07 +0200

    start ratings index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # course materials suggests: get 'ratings', to: 'ratings#index'
+  # this should be quivalent using resources
+#  get 'ratings', to: 'ratings#index'
+  resources :ratings, only: [:index]
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
