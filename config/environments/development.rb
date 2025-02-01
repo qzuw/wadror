3bef32e1d1e50df371c0f6b42a8a20f9894595b7
Terho Uotila <terho.uotila@helsinki.fi>
2025-02-01 20:40:23 +0200

    ip for console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # web console
+  config.web_console.permissions = '172.17.0.0/16'
 end
